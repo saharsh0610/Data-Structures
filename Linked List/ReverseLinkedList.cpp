cf1408776e726f357624c8b7cba04a51c0cab0ba
saharsh0610 <saharsh.gupta06@gmail.com>
2022-02-07 23:41:20 +0530

    Create ReverseLinkedList.cpp

---
+++ Linked List/ReverseLinkedList.cpp
@@ -0,0 +1,44 @@
+// Loop Method
+Node *reverse(Node *head)
+{
+    if (head == NULL || head->next == NULL)
+    {
+        return head;
+    }
+    Node *forward = NULL;
+    Node *prev = NULL;
+    Node *curr = head;
+
+    while (curr != NULL)
+    {
+        forward = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = forward;
+    }
+    return prev;
+}
+
+// Recursive Method
+
+void reverse(Node *&head, Node *curr, Node *prev)
+{
+    if (curr == NULL)
+    {
+        head = prev;
+        return;
+    }
+
+    Node *forward = curr->next;
+    reverse(head, forward, curr);
+    curr->next = prev;
+}
+
+Node *ReverseLinkedList(Node *head)
+{
+    Node *forward = NULL;
+    Node *prev = NULL;
+    Node *curr = head;
+    reverse(head, curr, prev);
+    return head;
+}
